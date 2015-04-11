ab6ebc38e74e1ece3d55059b0a0a905faf645f0c
Frederico Ferreira de Figueiredo <fredcomp@gmail.com>
2015-04-11 11:36:14 -0300

    Incluíndo mais alguns atributos
    Git não pegou essas mudanças no outro commit.

---
+++ src/Doutor.java
@@ -0,0 +1,7 @@
+public class Doutor extends Pesquisador {
+
+	private int numGradOrient;
+	private int numMestreOrient;
+	private int numDoutorOrient;
+	
+}
---
+++ src/Pesquisador.java
@@ -0,0 +1,9 @@
+public class Pesquisador { 
+
+	private int idPesquisador;
+	private int horaIniciacaoCientifica;
+	private int EstagioDocencia;
+	
+	private Artigo[] artigos;
+	
+}
